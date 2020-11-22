6416544a45121338ec5fb3713296c00344a19086
Maksimonza <maximus83onza@gmail.com>
2020-11-22 21:45:35 +0200

    first commit for task_1

---
+++ Task_1/src/main/java/com/Task_1/App.java
@@ -0,0 +1,13 @@
+package com.Task_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
